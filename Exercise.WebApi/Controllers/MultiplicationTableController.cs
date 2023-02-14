105556380a50706d3131c3209194cfd3a62e9eba
11202011 <11202011@AGI54000011>
2023-02-14 13:55:00 +0800

    加入專案檔案。

---
+++ Exercise.WebApi/Controllers/MultiplicationTableController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using Exercise.ClassLibrary;
+
+namespace Exercise.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MultiplicatissonTableController : ControllerBase
+    {
+        //[HttpPost("{number:int:range(2,9)}")]
+        [HttpPost]
+        [SwaggerOperation(Summary = "取得Multiplicandr的99乘法表",
+            Description = "透過Multiplicand取得99乘法表，Multiplicand限制為整數2-9，若Multiplicand為0或空取得2-9的99乘法表")]
+        public ReturnData<List<GetMultiplicationTableResult>> GetMultiplicationTable([FromBody] GetMultiplicationTableParameter parameter)
+        {   
+            int num = parameter.Multiplicand;
+            ReturnData<List<GetMultiplicationTableResult>> result = new ReturnData<List<GetMultiplicationTableResult>>();
+            result.Data = new List<GetMultiplicationTableResult>();
+            Boolean isSuccessful = (num >= 2 && num <= 9) || num == 0;
+            result.Code = isSuccessful ? 200 : 400;
+            result.Status = isSuccessful ? "Success." : "Failure.";
+            if (isSuccessful)
+            {
+                if (num == 0)
+                {
+                    for (int i = 2; i < 10; i++)
+                    {
+                        result.Data.Add(new GetMultiplicationTableResult(i));
+                    }
+                }
+                else
+                {
+                    result.Data.Add(new GetMultiplicationTableResult(num));
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Exercise.WebApi/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+    {
+        options.EnableAnnotations();
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Exercise API",
+            Description = "An ASP.NET Core Web API for exercise",
+        });
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    }
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(options =>
+    {
+        options.SerializeAsV2 = true;
+    });
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Exercise API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
